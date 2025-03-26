4cb38a45c53d6f329232fee0ec9b7dc97935e451
NotaHuman <dmitriy.busin@gmail.com>
2025-03-26 16:55:44 +0300

    updated structure

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -6,7 +6,6 @@ using Apllication4Course.Models;
 using Apllication4Course.Services;
 using System;
 using System.Windows.Controls;
-using Moq;
 using System.Linq;
 using System.Security.Policy;
 
@@ -46,7 +45,7 @@ namespace UnitTestProject
         [TestMethod]
         public void CheckRegister_UserAlreadyExists()
         {
-
+            // Arrange
             string login = "head_admin", password = "h272D_Ad!n", lastname = "Топазов", firstname = "Артём", position = "Администратор",
              phone = "+79804368641", fathername = "Григорьевич", email = "efrem3839@gmail.com";
 
@@ -73,7 +72,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(1, res); // Успешная регистрация
+            Assert.AreEqual(1, res); 
         }
 
         [TestMethod]
@@ -90,7 +89,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(1, res); // Успешная регистрация (без отчества и email)
+            Assert.AreEqual(1, res); 
         }
 
         [TestMethod]
@@ -107,7 +106,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-4, res); // Неверный формат телефона
+            Assert.AreEqual(-4, res); 
         }
 
         [TestMethod]
@@ -124,7 +123,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-4, res); // Неверный формат телефона
+            Assert.AreEqual(-4, res); 
         }
 
         [TestMethod]
@@ -141,7 +140,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-4, res); // Неверный формат телефона
+            Assert.AreEqual(-4, res); 
         }
 
         [TestMethod]
@@ -158,7 +157,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(1, res); // Успешная регистрация (без email)
+            Assert.AreEqual(1, res); 
         }
 
         [TestMethod]
@@ -175,7 +174,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(1, res); // Успешная регистрация (без отчества)
+            Assert.AreEqual(1, res);
         }
 
         [TestMethod]
@@ -192,7 +191,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-1, res); // Пустые поля
+            Assert.AreEqual(-1, res); 
         }
 
         [TestMethod]
@@ -209,7 +208,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-1, res); // Пустые поля (только пробелы)
+            Assert.AreEqual(-1, res);
         }
 
         [TestMethod]
@@ -226,7 +225,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-2, res); // Слабый пароль
+            Assert.AreEqual(-2, res); 
         }
 
         [TestMethod]
@@ -243,7 +242,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-3, res); // Неверный формат email
+            Assert.AreEqual(-3, res); 
         }
 
         [TestMethod]
@@ -260,7 +259,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-3, res); // Неверный формат email
+            Assert.AreEqual(-3, res); 
         }
 
         [TestMethod]
@@ -277,7 +276,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-3, res); // Неверный формат email
+            Assert.AreEqual(-3, res); 
         }
 
         [TestMethod]
@@ -294,7 +293,7 @@ namespace UnitTestProject
             int res = ToReg(un, ps, ln, fn, pos, ph, mn, em);
 
             // Assert
-            Assert.AreEqual(-3, res); // Неверный формат email
+            Assert.AreEqual(-3, res);
         }
 
         public void Dispose() => _context.Database.CurrentTransaction?.Rollback();
@@ -318,9 +317,14 @@ namespace UnitTestProject
         [TestMethod]
         public void ExsistingUser_1()
         {
+            // Arrange
             string login = "head_admin", password = "h272D_Ad!n";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 1);
 
         }
@@ -328,9 +332,14 @@ namespace UnitTestProject
         [TestMethod]
         public void ExsistingUser_2()
         {
+            // Arrange
             string login = "general_manager", password = "g272L_mAn!r";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 1);
 
         }
@@ -338,9 +347,14 @@ namespace UnitTestProject
         [TestMethod]
         public void ExsistingUser_3()
         {
+            // Arrange
             string login = "deputy_head", password = "d27P2y_h56D!a";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 1);
 
         }
@@ -348,9 +362,14 @@ namespace UnitTestProject
         [TestMethod]
         public void ExsistingUser_4()
         {
+            // Arrange
             string login = "head_pathologist", password = "h272D_pA9843o!s";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 1);
 
         }
@@ -358,9 +377,14 @@ namespace UnitTestProject
         [TestMethod]
         public void ExsistingUser_5()
         {
+            // Arrange
             string login = "head_assistant", password = "h272D_As63is!t";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 1);
 
         }
@@ -368,36 +392,56 @@ namespace UnitTestProject
         [TestMethod]
         public void LostLogin()
         {
+            // Arrange
             string login = "", password = "h272D_Ad!n";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, -1);
         }
 
         [TestMethod]
         public void BadLogin()
         {
+            // Arrange
             string login = "1231231323121221212", password = "h272D_Ad!n";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 0);
         }
 
         [TestMethod]
         public void LostPass()
         {
+            // Arrange
             string login = "head_admin", password = "";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, -1);
         }
 
         [TestMethod]
         public void BadPass()
         {
+            // Arrange
             string login = "head_admin", password = "12_+-PPPooodd_=12";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 0);
         }
 
@@ -406,164 +450,26 @@ namespace UnitTestProject
         {
             string login = "head_admin87878", password = "h272D_Ad!n89898";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, 0);
         }
 
         [TestMethod]
         public void SpacesCheck()
         {
+            // Arrange
             string login = "", password = "";
             Console.WriteLine($"Testing with Login: {login}, Password: {password}");
+
+            // Act
             int res = ToLogin(login, password);
+
+            // Assert
             Assert.AreEqual(res, -1);
         }
     }
-
-
-    /* NotWorking
-    [TestClass]
-    public class RegUnitTest
-    {
-        public int ToReg(string userName, string password,
-            string lastname, string firstname, string position,
-            string phone, string fathername, string email)
-        {
-            var page = new RegisterViewModel();
-            int res = CheckRegister(userName, password, lastname,
-                firstname, position, phone, fathername, email);
-            return res;
-        }
-
-        [TestMethod]
-        public void GoodFullRegOrCheckTheReg()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "Демидович", pos = "Лаборант", ph = "+70084091111", em = "IDMoroz@mail.com";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, 1);
-            Assert.AreEqual(res, -5);
-        }
-
-        [TestMethod]
-        public void GoodMainReg()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, 1);
-            Assert.AreEqual(res, -5);
-        }
-
-        [TestMethod]
-        public void BadPhomeReg_1()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "88888888888888", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -4);
-        }
-
-        [TestMethod]
-        public void BadPhomeReg_2()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+78888888888888898989", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -4);
-        }
-
-        [TestMethod]
-        public void BadPhomeReg_3()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+788", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -4);
-        }
-
-        [TestMethod]
-        public void GoodNoMailReg()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "Демидович", pos = "Лаборант", ph = "+70084091111", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, 1);
-            Assert.AreEqual(res, -5);
-        }
-
-        [TestMethod]
-        public void GoodNoMidnameReg()
-        {
-            string un = "lab_tech", ps = "L01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "antony@gmail.com";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, 1);
-            Assert.AreEqual(res, -5);
-        }
-
-        [TestMethod]
-        public void NullCheckReg()
-        {
-            string un = "", ps = "", ln = "", fn = "",
-                mn = "", pos = "Лаборант", ph = "", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -1);
-        }
-
-        [TestMethod]
-        public void SpacesCheckReg()
-        {
-            string un = " ", ps = " ", ln = " ", fn = " ",
-                mn = " ", pos = "Лаборант", ph = " ", em = " ";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -1);
-        }
-
-        [TestMethod]
-        public void EasyPassReg()
-        {
-            string un = "lab_tech", ps = "lab_tech123", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -2);
-        }
-
-        [TestMethod]
-        public void WrMailReg_1()
-        {
-            string un = "lab_tech", ps = "l01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "antony.gmail.com";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -3);
-        }
-
-        [TestMethod]
-        public void WrMailReg_2()
-        {
-            string un = "lab_tech", ps = "l01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "antony@gmailcom";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -3);
-        }
-
-        [TestMethod]
-        public void WrMailReg_3()
-        {
-            string un = "lab_tech", ps = "l01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "@gmail.com";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -3);
-        }
-
-        [TestMethod]
-        public void WrMailReg_4()
-        {
-            string un = "lab_tech", ps = "l01-b_T272!ch", ln = "Морозов", fn = "Игорь",
-                mn = "", pos = "Лаборант", ph = "+70084091111", em = "antony@gmail";
-            int res = ToReg(un, ps, ln, fn, ps, ph, mn, em);
-            Assert.AreEqual(res, -3);
-        }
-    }
-    */
 }
