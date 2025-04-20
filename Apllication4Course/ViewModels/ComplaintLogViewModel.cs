74a2a09b351da12ff26ae0770328202c32ddebb5
NotaHuman <dmitriy.busin@gmail.com>
2025-04-20 12:02:15 +0300

    search, sort and filtration

--- Apllication4Course/ViewModels/ComplaintLogViewModel.cs
+++ Apllication4Course/ViewModels/ComplaintLogViewModel.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections.ObjectModel;
-using Apllication4Course.Models;
+﻿using Apllication4Course.Models;
+using Apllication4Course.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using System;
 
-namespace Apllication4Course.ViewModels
+public class ComplaintLogViewModel : SearchSortViewModel<Журнал_Обращений>
 {
-    public class ComplaintLogViewModel : BaseDataViewModel<Журнал_Обращений>
-    {
-        public ObservableCollection<Журнал_Обращений> ComplaintLogs => Items;
+    public ObservableCollection<Журнал_Обращений> ComplaintLogs => Items;
 
-        public ComplaintLogViewModel()
+    public ComplaintLogViewModel()
+    {
+        AddCommand = new RelayCommand(AddNewItem);
+        EditCommand = new RelayCommand(EditSelectedItem, () => IsEditEnabled);
+        DeleteCommand = new RelayCommand(DeleteSelectedItem, () => IsDeleteEnabled);
+        SaveCommand = new RelayCommand(SaveChanges);
+
+    }
+
+    protected override Dictionary<string, Expression<Func<Журнал_Обращений, object>>> CreateSortProperties()
+    {
+        return new Dictionary<string, Expression<Func<Журнал_Обращений, object>>>
         {
-            AddCommand = new RelayCommand(AddNewItem);
-            EditCommand = new RelayCommand(EditSelectedItem, () => IsEditEnabled);
-            DeleteCommand = new RelayCommand(DeleteSelectedItem, () => IsDeleteEnabled);
-            SaveCommand = new RelayCommand(SaveChanges);
-        }
+            ["ФИО"] = x => x.ФИО_Обратившегося,
+            ["Дата обращения"] = x => x.Дата_Обращения,
+            ["Тип обращения"] = x => x.Тип_Обращения
+        };
+    }
+
+    protected override bool FilterPredicate(Журнал_Обращений item)
+    {
+        if (string.IsNullOrWhiteSpace(SearchText))
+            return true;
+
+        // Ищем по основным полям (регистронезависимо)
+        return (item.ФИО_Обратившегося != null && item.ФИО_Обратившегося.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0) ||
+               (item.КонтактныйТелефон != null && item.КонтактныйТелефон.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0) ||
+               (item.Тип_Обращения != null && item.Тип_Обращения.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0);
     }
 }
---
+++ Apllication4Course/ViewModels/SearchSortViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Apllication4Course.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows.Input;
+using System;
+using System.Linq;
+
+public abstract class SearchSortViewModel<T> : BaseDataViewModel<T> where T : class, new()
+{
+    private string _searchText;
+    private string _selectedSortProperty;
+    private ListSortDirection _sortDirection = ListSortDirection.Ascending;
+    private Dictionary<string, Expression<Func<T, object>>> _sortProperties;
+
+    public string SearchText
+    {
+        get => _searchText;
+        set => Set(ref _searchText, value);
+    }
+
+    public string SelectedSortProperty
+    {
+        get => _selectedSortProperty;
+        set => Set(ref _selectedSortProperty, value);
+    }
+
+    public ListSortDirection SortDirection
+    {
+        get => _sortDirection;
+        set => Set(ref _sortDirection, value);
+    }
+
+    public ObservableCollection<string> SortProperties { get; }
+    public ICommand ApplyFilterCommand { get; }
+    public ICommand SortAscendingCommand { get; protected set; }
+    public ICommand SortDescendingCommand { get; protected set; }
+
+    protected SearchSortViewModel()
+    {
+        _sortProperties = CreateSortProperties();
+        SortProperties = new ObservableCollection<string>(_sortProperties.Keys);
+        SelectedSortProperty = SortProperties.FirstOrDefault();
+
+        ApplyFilterCommand = new RelayCommand(ApplyFilterAndSort);
+        SortAscendingCommand = new RelayCommand(() =>
+        {
+            SortDirection = ListSortDirection.Ascending;
+            ApplyFilterAndSort();
+        });
+
+        SortDescendingCommand = new RelayCommand(() =>
+        {
+            SortDirection = ListSortDirection.Descending;
+            ApplyFilterAndSort();
+        });
+    }
+
+    protected abstract Dictionary<string, Expression<Func<T, object>>> CreateSortProperties();
+    protected abstract bool FilterPredicate(T item);
+
+    protected virtual void ApplyFilterAndSort()
+    {
+        if (_items == null) return;
+
+        var result = new ObservableCollection<T>();
+
+        // Применяем фильтрацию
+        foreach (var item in _items)
+        {
+            if (string.IsNullOrWhiteSpace(SearchText) || FilterPredicate(item))
+                result.Add(item);
+        }
+
+        // Применяем сортировку
+        if (_sortProperties.TryGetValue(SelectedSortProperty, out var sortExpression))
+        {
+            var sorted = SortDirection == ListSortDirection.Ascending
+                ? result.OrderBy(sortExpression.Compile())
+                : result.OrderByDescending(sortExpression.Compile());
+
+            result = new ObservableCollection<T>(sorted);
+        }
+
+        // Полностью заменяем коллекцию Items
+        Items.Clear();
+        foreach (var item in result)
+            Items.Add(item);
+
+        OnPropertyChanged(nameof(Items));
+    }
+
+    protected override void LoadData() => base.LoadData();
+}
