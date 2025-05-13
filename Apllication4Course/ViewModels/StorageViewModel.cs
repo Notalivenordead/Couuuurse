3f8d2c28d53b84b0384da197352c504f85d6192f
NotaHuman <dmitriy.busin@gmail.com>
2025-05-13 12:07:36 +0300

    Debugged

--- Apllication4Course/ViewModels/StorageViewModel.cs
+++ Apllication4Course/ViewModels/StorageViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq.Expressions;
 using Apllication4Course.Models;
 
 namespace Apllication4Course.ViewModels
 {
-    public class StorageViewModel : BaseDataViewModel<Камеры_Хранения>
+    public class StorageViewModel : SearchSortViewModel<Камеры_Хранения>
     {
         public ObservableCollection<Камеры_Хранения> Storage => Items;
 
@@ -13,6 +17,52 @@ namespace Apllication4Course.ViewModels
             EditCommand = new RelayCommand(EditSelectedItem, () => IsEditEnabled);
             DeleteCommand = new RelayCommand(DeleteSelectedItem, () => IsDeleteEnabled);
             SaveCommand = new RelayCommand(SaveChanges);
+        }
+
+        protected override Dictionary<string, Expression<Func<Камеры_Хранения, object>>> CreateSortProperties()
+        {
+            return new Dictionary<string, Expression<Func<Камеры_Хранения, object>>>
+            {
+                ["Температура"] = x => x.Температура,
+                ["Номер_Камеры"] = x => x.Номер_Камеры
+            };
+        }
+
+        protected override bool FilterPredicate(Камеры_Хранения item)
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+                return true;
+
+            // Поиск по температуре
+            if (item != null)
+            {
+                // Точное совпадение числа
+                if (decimal.TryParse(SearchText, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal searchTemp))
+                {
+                    if (item.Температура == searchTemp)
+                        return true;
+                }
+
+                // Поиск в строковом представлении
+                if (item.Температура.ToString(CultureInfo.InvariantCulture).Contains(SearchText))
+                    return true;
+            }
+
+            // Поиск по номеру камеры
+            if (item != null)
+            {
+                // Точное совпадение числа
+                if (decimal.TryParse(SearchText, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal searchTemp))
+                {
+                    if (item.Номер_Камеры == searchTemp)
+                        return true;
+                }
+
+                // Поиск в строковом представлении
+                if (item.Номер_Камеры.ToString(CultureInfo.InvariantCulture).Contains(SearchText))
+                    return true;
+            }
+            return false;
         }
     }
 }
